b074d59b7a973990937340295a7de72ae8525f07
Leandro Andrade <113956647+leanttro@users.noreply.github.com>
2026-06-20 18:37:16 -0300

    Delete backend directory

--- backend/app/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from app.models.client import Client
-from app.models.project import Project
-from app.models.version import Version
-from app.models.comment import Comment
-from app.models.asset import Asset
-from app.models.approval import Approval
